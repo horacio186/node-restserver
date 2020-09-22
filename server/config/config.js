02aba10f9df7e97c1fb2821423e65ecacaf726bc
Horacio ortega <ortega.farina@gmail.com>
2020-09-22 16:59:26 -0300

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://horacio:AhnFfkqeMVN1NPbj@cluster0.ru2cw.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
